dd356c59d8d8edd523f38e873510c327006884f4
Dale Pepper <dale_pepper@hotail.co.uk>
2018-02-22 22:22:33 +1100

    Added dummy response

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 //Set up app and middleware
 
+//Import modules
 const express = require('express');
 
 const app = express(); //Create new express app
+
+app.get('/', (req, res) => {
+    res.json({
+        text: "Hello World!"
+    });
+});
 
 module.exports = app; //Using module.exports as we want access to the entire app.
--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const app = require('./app'); //Bring in app
 
+// Set port and start app
 app.set('port', 8888);
 app.listen(app.get('port'), () => {
     console.log(`Express running -> PORT ${app.get('port')}`);
