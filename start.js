f3d6342759a5c6480724d91bf12c4ed327677627
Dale Pepper <dale_pepper@hotail.co.uk>
2018-02-24 17:29:43 +1100

    Added environment variables folder

--- start.js
+++ start.js
@@ -1,7 +1,11 @@
 const app = require('./app'); //Bring in app
+const dotenv = require('dotenv');
+
+//import enviroment variables from file
+dotenv.config({ path: 'environment-variables.env' });
 
 // Set port and start app
-app.set('port', 8888);
+app.set('port', process.env.PORT);
 app.listen(app.get('port'), () => {
     console.log(`Express running -> PORT ${app.get('port')}`);
 });
