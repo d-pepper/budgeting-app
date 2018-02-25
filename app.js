f1780af86528f75c268610cd6f3a65f96f11be13
Dale Pepper <dale_pepper@hotail.co.uk>
2018-02-26 00:33:51 +1100

    Added mongoose. Connected to local db. Return dummy budget repsonse

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const express = require('express');
 
 const app = express(); //Create new express app
 
+const budget = {
+    income: [
+        { name: "Work", amount: "8000" }
+    ],
+    outgoing: [
+        { name: "Rent", amount: "1000" },
+        { name: "Netflix", amount: "10" },
+        { name: "Food", amount: "200" }
+    ]
+};
+
 app.get('/', (req, res) => {
-    res.json({
-        text: "Hello World!"
-    });
+    res.json(budget);
 });
 
 module.exports = app; //Using module.exports as we want access to the entire app.
--- start.js
+++ start.js
@@ -1,8 +1,15 @@
 const app = require('./app'); //Bring in app
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 //import enviroment variables from file
 dotenv.config({ path: 'environment-variables.env' });
+
+//Connect to db and handle errors
+mongoose.connect(process.env.DATABASE);
+mongoose.connection.on('error', (err) => {
+  console.error(`${err.message}`);
+});
 
 // Set port and start app
 app.set('port', process.env.PORT);
